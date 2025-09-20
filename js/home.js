f6a8cfb2129055b4d81fa77751bf351fffc9b89c
Ashikmridha793 <ashikmridha793@gmail.com>
2025-09-21 00:09:12 +0600

    added parshiali event handlar

--- js/home.js
+++ js/home.js
@@ -1,1 +1,16 @@
-console.log('home page js file added')
+// add money to the account
+
+// step-1: add an event handler to the add money button inside the form
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+      // prevent page reload after form submission
+      event.preventDefault();
+      
+      // step-2:
+      const addMonyInput = document.getElementById('input-add-money').value
+      console.log(addMonyInput);
+
+      // get the pin number
+      const pinNumber = document.getElementById('input-pin-number').value
+      console.log(pinNumber);
+
+})
