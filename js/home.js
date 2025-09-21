e82424a9ac76a76a79652790750101745298d497
Ashikmridha793 <ashikmridha793@gmail.com>
2025-09-21 18:29:41 +0600

    added some calculation to add money

--- js/home.js
+++ js/home.js
@@ -11,6 +11,27 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
 
       // get the pin number
       const pinNumber = document.getElementById('input-pin-number').value
-      console.log(pinNumber);
+      console.log(typeof pinNumber);
+
+      // step -3: validate pin number
+      if (pinNumber === '1234'){
+            console.log('adding money to your account')
+
+            // step-4: get the current balance
+            const balance  = document.getElementById('account-balance').innerText
+            console.log(typeof balance);
+
+            // step-5: add addMonyInput to the current balance
+            const addMonyNumber = parseFloat(addMonyInput)
+            const balanceNumber = parseFloat(balance)
+            const newbalance = addMonyNumber + balanceNumber
+            console.log(newbalance);
+            
+            // step-6: update the balance in the UI/DOM
+            document.getElementById('account-balance').innerText = newbalance
+      }
+      else{
+            alert('failed to add money')
+      }
 
 })
